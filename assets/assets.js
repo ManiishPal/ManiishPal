ee1981253e22735c3567400199fcd424d7b29e28
Manish Pal <manish8872pal@gmail.com>
2026-09-11 14:27:55 +0530

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,17 @@
+import githubImage from "./github-image for readme - Copy.gif";
+import ancestor5 from "./ancestor5 - Copy.gif";
+import ancestor4 from "./ancestor4 - Copy.gif";
+import ancestor3 from "./ancestor3 - Copy.gif";
+import ancestor2 from "./ancestor2 - Copy.gif";
+import ancestor from "./ancestor - Copy.gif";
+
+const assets = {
+  githubImage,
+  ancestor5,
+  ancestor4,
+  ancestor3,
+  ancestor2,
+  ancestor,
+};
+
+export default assets;
